48d0370e77aecd0841801e408ab5345b56426d7e
GarethHolloway <33927441+GarethHolloway@users.noreply.github.com>
2019-03-06 15:04:39 +0000

    UPDATED gyroscope.py
    made code more efficient, reduced variables, editted variable names. Working.

--- Gyroscope/gyroscope.py
+++ Gyroscope/gyroscope.py
@@ -2,7 +2,7 @@
 
 import smbus
 
-deviceAddr = 0x68   # Identify this address, needs to be changed, can use I2Cdetect on the Pi.
+deviceAddr = 0x68   # Correct device address.
 PWR_MGMT_1   = 0x6B #Need to check.
 SMPLRT_DIV   = 0x19 #Need to check.
 CONFIG       = 0x1A #Need to check.
@@ -21,30 +21,26 @@ def setup(devAddress, bus):
 	
 def readData(devAddress, address, busConfig):
 	#Gyro is 16 bit, 2 bytes of information, one value will be used, the other dropped based on value presence<Or gate principals>.
-        high = busConfig.read_byte_data(devAddress, address) 
-        low = busConfig.read_byte_data(devAddress, address+1)
+        upperValue = busConfig.read_byte_data(devAddress, address) 
+        lowerValue = busConfig.read_byte_data(devAddress, address+1)
     
         # Will join the values together used OR gate principal, if present in either = 1, else = 0.
-        value = ((high << 8) | low) # Bitwise Or statement, << means to move bits 8 places to the left(Clears the register byte).
+        joinUpperandLower = ((upperValue << 8) | lowerValue) # Bitwise Or statement, << means to move bits 8 places to the left(Clears the register byte).
         
         #Retrieves signed form of the value.
-        if(value > 32768): # 16bit devices have a range of -32768 to +32767 so this acts as a Analog to Digital converter.
-                value = value - 65536
-        return value
+        if(joinUpperandLower > 32768): # 16bit devices have a range of -32768 to +32767 so this acts as a Analog to Digital converter.
+                joinUpperandLower = joinUpperandLower - 65536
+        return joinUpperandLower
 
 def runner(device):
 	
 	bus = smbus.SMBus(1) # Establish device connection with the serial bus.
 	setup(device, bus)
-		
 	sensitivity = 131.00  #Sensitivity divisor, utilises LSB, is the most accurate/sensitive the gyroscope module can be.
-	readGyroX = readData(device, xGyro, bus)
-	readGyroY = readData(device, yGyro, bus)
-	readGyroZ = readData(device, zGyro, bus)
 	
-	gyroXValue = readGyroX/sensitivity
-	gyroYValue = readGyroY/sensitivity
-	gyroZValue = readGyroZ/sensitivity 
+	gyroXValue = (readData(device, xGyro, bus))/sensitivity
+	gyroYValue = (readData(device, xGyro, bus))/sensitivity
+	gyroZValue = (readData(device, xGyro, bus))/sensitivity 
 	
 
 	print ("X Gyro= " + str(gyroXValue) + " Degrees per second" + " " + "Y Gyro= " + str(gyroYValue) + " Degrees per second" + " " + "Z Gyro= " + str(gyroZValue) + " Degrees per second")
