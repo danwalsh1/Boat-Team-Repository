14bd9a96c14f3e149cdfff4f0d04b628131eb424
GarethHolloway <33927441+GarethHolloway@users.noreply.github.com>
2019-03-07 21:31:39 +0000

    UPDATED gyroscope.py
    Updated comments.

--- Gyroscope/gyroscope.py
+++ Gyroscope/gyroscope.py
@@ -27,28 +27,32 @@ def setup(devAddress, sysBus)
 	sysBus.write_byte_data(devAddress, INT_ENABLE, 1) # Set interrupts to be possible in the interrupt register.
 	
 def readData(devAddress, address, busConfig):
-	#Gyro is 16 bit, join the values together using an or gate. Address is the mem registers hex ID. devAddress is the physical gyroscope.
-        upperValue = busConfig.read_byte_data(devAddress, address) 
-        lowerValue = busConfig.read_byte_data(devAddress, address+1)
+	# Gyro is 16 bit, join the values together using an or gate. 
+	# Address is the mem registers hex ID. devAddress is the physical gyroscope.
+        register1Higher = busConfig.read_byte_data(devAddress, address) 
+        register2Lower = busConfig.read_byte_data(devAddress, address+1)
     
-        # Will join the bit values together using an OR gate I.e a 1 present in either register equals 1 as output, else = 0.
-        joinUpperandLower = ((upperValue << 8) | lowerValue) # Bitwise Or statement on both registers.  << means to move bits 8 places to the left while calculating or gate value.
+        # Will join the bit values together using an OR gate
+	# As a representation a 1 present in either register equals 1 as output, else = 0.
+        joinHigherAndLower = ((register1Higher << 8) | register2Lower) # Bitwise Or statement on 16 bit register.  << means to move bits 8 places to the left while calculating or gate value.
 	 
-        #Retrieves signed form of the value.
-        if(joinUpperandLower > 32768): # 16bit devices have a range of -32768 to +32767 so this acts as a converter from signed to unsigned.
-                joinUpperandLower = joinUpperandLower - 65536
-        return joinUpperandLower
+        if(joinHigherAndLower < 32768): # 16bit devices have a range of -32768 to +32767 so this acts as a converter from signed to unsigned.    
+        	return joinHigherAndLower # Signed Value returned.
+	else:
+		joinHigherAndLower = joinHigherAndLower - 65536
+		return joinHigherAndLower # Signed Value returned.
 
 def runner(device):
 	try:
-            bus = smbus.SMBus(1) # Establish device connection with the serial bus.
+            bus = smbus.SMBus(1) # Establish device connection with the serial bus at I2C position 1(I2C1).
             setup(device, bus)
             sensitivity = 131.00  #Sensitivity divisor, utilises LSB, this sets the angular velocity to 250 degrees per second(most sensitive setting).
 
             gyroXValue = (readData(device, xGyro, bus))/sensitivity
             gyroYValue = (readData(device, xGyro, bus))/sensitivity
             gyroZValue = (readData(device, xGyro, bus))/sensitivity
-            print ("X Gyro= " + str(gyroXValue) + " Degrees per second" + " " + "Y Gyro= " + str(gyroYValue) + " Degrees per second" + " " + "Z Gyro= " + str(gyroZValue) + " Degrees per second")
+            
+	    print ("X Gyro= " + str(gyroXValue) + " Degrees per second" + " " + "Y Gyro= " + str(gyroYValue) + " Degrees per second" + " " + "Z Gyro= " + str(gyroZValue) + " Degrees per second")
 
         except:
             print ("Error")
