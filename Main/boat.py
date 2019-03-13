946d4d4f2600577920bf945fc71cd630cdccae4f
AndyHills378 <47143482+AndyHills378@users.noreply.github.com>
2019-03-13 14:34:06 +0000

    CREATED boat.py
    Created a function to concatenate all work that had been done prior to the creation of this file

---
+++ Main/boat.py
@@ -0,0 +1,39 @@
+import Main from New_compass_code.py #Compass Input
+import retrieveGPS from RetrieveGPS.py #GPS Input
+import gyroMain from gyroscope.py #Gyroscope Output
+import RudderMain from RudderMove.py #Rudder Output
+import sailMain from sail.py #Sail Output
+#import compassMain from compass.py #compass
+
+Lat, Long = 0
+startHeading = [Lat, Long]
+
+def concatenateMain():
+    print ("To stop the script press LCTRL + C")
+    startInput = input("Press y to start the script")
+    if startInput in ["Y", "y"]:
+        boolRunning = True
+        while boolRunning:
+            try:
+                interpretDirection(retrieveGPS(), Compass) #return value from retrieveGPS used as Parameter
+                gyroMain()
+            except KeyboardInterrupt:
+                boolRunning = False
+                print ("Program closing")
+                exit()
+                
+    else:
+        print ("Program closing")
+        exit()
+
+def interpretDirection(GPS, Compass):
+    """Nav function that uses the GPS and compass to determine location"""
+    if:
+        #heading is correct
+    else:
+        #heading is false
+        RudderMain()
+        sailMain()
+
+
+concatenateMain()
